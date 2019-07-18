bf35c29523c654aeaa03239ce33446bd1defb9d8
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-07-18 16:37:45 -0400

    Checkpoint-3: Working on POST route

---
+++ src/db/migrations/20190716202517-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: { msg: "must be a valid email" }
+        }
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ src/routes/users.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const validation = require("./validation");
+
+router.get("/users/signup", userController.signUp);
+// router.post("/users/signup", userController.signUp);
+router.post("/users", validation.validateUsers, userController.create);
+
+module.exports = router;
