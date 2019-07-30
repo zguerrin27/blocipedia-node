147c7121d475fc2853c5c73dc089c85421d265c9
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-07-30 19:09:49 -0400

    Checkpoint-10: Completed. Added Markdown ability

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -1,5 +1,6 @@
 const wikiQueries = require("../db/queries.wikis.js");
 const Authorizer = require("../policies/wiki");
+const markdown = require("markdown").markdown;
 
 
 module.exports = {
@@ -49,7 +50,8 @@ module.exports = {
       if(err || wiki == null){
         res.redirect(404, "/");
       } else {
-        res.render("wikis/show", {wiki});
+        let content = markdown.toHTML(wiki.body);
+        res.render("wikis/show", {wiki, content});
       }
     });
   },
